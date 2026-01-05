f3697b60770e131a01abffe9d8a9ed0389c4b3b5
Arca Cengiz <arcaegecengiz@gmail.com>
2026-01-05 18:03:00 +0000

    First commit

---
+++ src/routes/fly/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  let accel = $state(0);
+  let maxAccel = $state(0);
+
+  onMount(() => {
+    function handleMotionEvent(event: any) {
+      accel = Math.sqrt(
+        event.acceleration.x ** 2 +
+          event.acceleration.y ** 2 +
+          event.acceleration.z ** 2
+      );
+
+      if (accel > maxAccel) {
+        maxAccel = accel;
+      }
+    }
+
+    window.addEventListener("devicemotion", handleMotionEvent);
+  });
+</script>
+
+<h1>Blahaj chucking thing</h1>
+
+accel: {accel}
+<br />
+max accel: {maxAccel}
