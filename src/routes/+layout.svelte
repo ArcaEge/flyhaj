4edcbaf70ba6cb9a13743dd0fe72042ea72a83b8
Arca Cengiz <arcaegecengiz@gmail.com>
2026-01-05 18:13:08 +0000

    Add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
@@ -8,5 +9,4 @@
 	<link rel="icon" href={favicon} />
 	<title>Flyhaj</title>
 </svelte:head>
-
 {@render children()}
