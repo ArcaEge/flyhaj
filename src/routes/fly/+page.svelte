d54f372aca9750b3622a9b419f9544cc17f8f755
Arca Cengiz <arcaegecengiz@gmail.com>
2026-01-06 11:34:18 +0100

    Add jerk measurements

--- src/routes/fly/+page.svelte
+++ src/routes/fly/+page.svelte
@@ -2,8 +2,13 @@
   import { onMount } from "svelte";
   import Quaternion from "quaternion";
 
-  let accel = $state(0);
+  let accel: number | null = $state(null);
   let maxAccel = $state(0);
+  let accelTime = $state(0);
+
+  let jerk = $state(0);
+  let maxJerk = $state(0);
+  let firstJerk = $state(0);
 
   let angleAlpha = $state(0);
   let angleBeta = $state(0);
@@ -27,16 +32,23 @@
   let velZMax = $state(0);
   let velOverallMax = $state(0);
 
-  onMount(() => {
+  let oscillator: OscillatorNode | null = $state(null);
+  let oscillatorState = false;
+
+  onMount(async () => {
+    const audioCtx = new AudioContext();
+
     function handleMotionEvent(event: any) {
-      accel = Math.sqrt(
+      const currentAccel = Math.sqrt(
         event.acceleration.x ** 2 +
           event.acceleration.y ** 2 +
           event.acceleration.z ** 2
       );
 
-      if (accel > maxAccel) {
-        maxAccel = accel;
+      if (accel !== null) {
+        const lastTime = accelTime;
+        accelTime = Date.now();
+        jerk = Math.abs(currentAccel - accel) * ((accelTime - lastTime) / 1000);
       }
 
       // Magic quarternion shit to calculate global acceleration
@@ -55,6 +67,26 @@
       rotatedAccelX = accelX;
       rotatedAccelY = accelY;
       rotatedAccelZ = accelZ;
+
+      // console.log(currentAccel);
+
+      if (7 <= currentAccel && currentAccel <= 13) {
+        if (!oscillatorState) {
+          oscillator = audioCtx.createOscillator();
+
+          oscillator.type = "square";
+          oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
+          oscillator.connect(audioCtx.destination);
+
+          oscillator.start();
+          oscillatorState = true;
+        }
+      } else {
+        if (oscillatorState && oscillator) {
+          oscillator.stop();
+          oscillatorState = false;
+        }
+      }
 
       if (Math.abs(rotatedAccelX) > rotatedAccelXMax) {
         rotatedAccelXMax = Math.abs(rotatedAccelX);
@@ -84,6 +116,19 @@
         velOverallMax = Math.abs(velOverall);
       }
 
+      accel = currentAccel;
+
+      if (accel > maxAccel) {
+        maxAccel = accel;
+      }
+      // jank asf
+      if (jerk > maxJerk && jerk < 100000) {
+        maxJerk = jerk;
+      }
+      if (jerk > 0.1 && firstJerk === 0 && accel < maxAccel - 0.1 && jerk < 100000) {
+        firstJerk = maxJerk;
+      }
+
       // velX += event.accelerationIncludingGravity.x * (event.interval / 1000);
       // velY += event.accelerationIncludingGravity.y * (event.interval / 1000);
       // velZ += event.accelerationIncludingGravity.z * (event.interval / 1000);
@@ -100,40 +145,53 @@
   });
 </script>
 
-<h1>Blahaj chucking thing</h1>
+<div class="m-2">
+  <h2>Blahaj chucking thing</h2>
 
-accel: {accel}
-<br />
-max accel: {maxAccel}
-<br />
-<br />
-alpha: {angleAlpha}
-<br />
-beta: {angleBeta}
-<br />
-gamma: {angleGamma}
-<br />
-<br />
-x max: {rotatedAccelXMax}
-<br />
-y max: {rotatedAccelYMax}
-<br />
-z max: {rotatedAccelZMax}
-<br />
-<br />
-x velocity: {velX}
-<br />
-y velocity: {velY}
-<br />
-z velocity: {velZ}
-<br />
-overall velocity: {velOverall}
-<br />
-<br />
-x velocity max: {velXMax}
-<br />
-y velocity max: {velYMax}
-<br />
-z velocity max: {velZMax}
-<br />
-overall velocity max: {velOverallMax}
+  <button class="block bg-amber-900 text-amber-50 p-2 my-2 rounded-lg"
+    >do the whee whee</button
+  >
+
+  accel: {accel}
+  <br />
+  max accel: {maxAccel}
+  <br />
+  <br />
+  jerk: {jerk}
+  <br />
+  max jerk: {maxJerk}
+  <br />
+  first jerk: {firstJerk}
+  <br />
+  <br />
+  alpha: {angleAlpha}
+  <br />
+  beta: {angleBeta}
+  <br />
+  gamma: {angleGamma}
+  <br />
+  <br />
+  x max: {rotatedAccelXMax}
+  <br />
+  y max: {rotatedAccelYMax}
+  <br />
+  z max: {rotatedAccelZMax}
+  <br />
+  <br />
+  x velocity: {velX}
+  <br />
+  y velocity: {velY}
+  <br />
+  z velocity: {velZ}
+  <br />
+  overall velocity: {velOverall}
+  <br />
+  <br />
+  x velocity max: {velXMax}
+  <br />
+  y velocity max: {velYMax}
+  <br />
+  z velocity max: {velZMax}
+  <br />
+  overall velocity max: {velOverallMax}
+</div>
