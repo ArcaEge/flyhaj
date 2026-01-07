421188eb8d55a67e55f148749c5ec3aef5c2eed9
Nate <daviesnathanael45@gmail.com>
2026-01-07 09:47:45 +0100

    leaderboard stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,17 @@
 <script>
     import header from "$lib/assets/header.png";
     import globe from "$lib/assets/globe.png";
-    import space from "$lib/assets/space.jpg"
 </script>
 
 <div class="flex justify-center">
     <img class="w-150 pt-15" src={header}/>
 </div>
 
-
 <div class="fixed bottom-0">
     <img class="w-2000" src={globe}/>
+</div>
+
+<div class="fixed left-10 top-10 bg-blue-800 h-100 rounded-xl border border-blue-400">
+    <h1 class=" p-5 font-sans font-extrabold text-blue-400">LeaderBoard</h1>
+    <hr class="w-100% text-blue-400">
 </div>
