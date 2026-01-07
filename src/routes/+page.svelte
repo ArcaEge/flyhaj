f1d5631ac19db3339d8d678e6573971a5fff0c6e
GingerGigiCat <gingerisacat@outlook.com>
2026-01-08 00:53:00 +0100

    frontend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,39 +18,41 @@
 		state = 'idle';
 	});
 </script>
+<div class="flex flex-row justify-between"></div>
+	<div class="min-h-screen bg-[url('../lib/assets/space.jpg')] bg-center bg-no-repeat flex flex-row justify-between">
+		<div>
+			<div class="top-25">
+				<img class="w-150" src={header} />
+			</div>
 
-<div class="min-h-screen bg-[url('../lib/assets/space.jpg')] bg-cover bg-center bg-no-repeat">
-	<div class="flex justify-center">
-		<img class="w-150 pt-15" src={header} />
-	</div>
-
-	<div class="absolute bottom-0">
-		<img class="w-2000" src={globe} />
-	</div>
-</div>
-
-<div class="absolute bottom-0">
-	<img class="w-2000" src={globe} />
-</div>
-
-<div class="absolute top-0 p-10 w-full">
-	<div class="flex flex-row justify-between">
-		<div class="bg-blue-800 h-100 rounded-xl border border-blue-400">
-			<h1 class="p-5 font-sans font-extrabold text-blue-400">Leaderboard</h1>
-			<hr class="w-100% text-blue-400" />
+			<img class="bottom-0 h-1/2 p-10" src={globe} />
 		</div>
-		<div class="bg-blue-800 h-100 rounded-xl border border-blue-400">
-			<h1 class="p-5 font-sans font-extrabold text-blue-400">Clicky stuff</h1>
-			<hr class="w-100% text-blue-400" />
-			<div class="grid grid-cols-2 p-3 gap-3">
-				<button
-					class="button"
-					onclick={() => {
-						socket.emit('reset');
-					}}>Reset phone state</button
-				>
-				<p class="text-lg text-blue-50 flex justify-center items-center">State: {state}</p>
+
+
+		<div class="absolute right-0 p-10 h-full">
+			<div class="flex flex-col justify-between">
+				<div class="bg-blue-800 h-90 rounded-xl border border-blue-400">
+					<h1 class="p-5 font-sans font-extrabold text-blue-400">Leaderboard</h1>
+					<hr class="w-100% text-blue-400" />
+				</div>
+				<div class="h-10"/>
+				<div class="bg-blue-800 h-90 rounded-xl border border-blue-400">
+					<h1 class="p-5 font-sans font-extrabold text-blue-400">Clicky stuff</h1>
+					<hr class="w-100% text-blue-400" />
+					<div class="grid grid-cols-2 p-3 gap-3">
+						<button
+							class="button"
+							onclick={() => {
+								socket.emit('reset');
+							}}>Reset phone state</button
+						>
+						<p class="text-lg text-blue-50 flex justify-center items-center">State: {state}</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+
+
+	
+
